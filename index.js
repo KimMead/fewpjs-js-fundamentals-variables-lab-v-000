7f6df837a5634b979d217009ebdcd2bbdfac30a6
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-11-11 02:22:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
-let mostProfitableNNeighborhood = 'Chelsea';
+let mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = 'Susan Smith';
